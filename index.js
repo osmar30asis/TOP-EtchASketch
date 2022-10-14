417d8fabc5bbd81752fb28cc0e06e61320517d27
osmaryupanqui <osmar30asis@gmail.com>
2022-10-13 20:51:40 -0500

    added button and random colors

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const container = document.querySelector('.container');
 
-const test = document.createElement("test");
-test.setAttribute('style', 'color: red');
-
-
 function addTiles (col, row) {
     const numTiles = col * row;
+    container.innerHTML = ''
     for (let i = 0; i < numTiles; i++) {
         const tileWidth = Number(100/col);
         const tileHeight = Number(100/row);
 
         const tile = document.createElement("tile");
+
         container.appendChild(tile);
         tile.setAttribute('class', 'tile ' + String(i + 1));
         tile.setAttribute('style', `box-sizing: border-box; width: ${tileWidth}%;
         height: ${tileHeight}%; border-style: solid; border-width: 2px`);
-
+        
     }
-    
+
+    const tiles = document.querySelectorAll('.tile');
+
+    tiles.forEach(function (tile) {
+        tile.addEventListener('mouseover', function () {
+            let randomR = Math.round(Math.random() * 255);
+            let randomG = Math.round(Math.random() * 255);
+            let randomB = Math.round(Math.random() * 255);
+            tile.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+        })
+    })
 }
 
-addTiles(4, 4);
+const btn = document.querySelector('.button-box');
+
+btn.addEventListener('click', function () {
+    const colNumber = document.querySelector('#Columns').value
+    const rowNumber = document.querySelector('#Rows').value
+
+    if (colNumber > 64 || rowNumber > 64) {
+        alert('Too many cells!!!')
+    }
+
+    addTiles(colNumber, rowNumber);
+})
